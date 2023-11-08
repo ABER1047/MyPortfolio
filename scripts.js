a97558329a4201ba658d5d468883a1c72bc16877
ABER <1047asd44@gmail.com>
2023-11-09 00:05:58 +0900

    2023_11_09

--- scripts.js
+++ scripts.js
@@ -906,9 +906,9 @@ function repos_works_gif()
 {
     scroll_animation[3] = 0;
 
-    var works_url = ["https://github.com/ABER1047/Just_Wak_and_Beats","https://github.com/ABER1047/bocchi","https://github.com/ABER1047/delaunay-trianglulation","https://github.com/ABER1047/HitoriBocchi"];
-    var works_title_eng = ["Just Wak & Beats","Bocchi!","Delaunay Triangulation","Hitori Bocchi","Empty"];
-    var works_title_kr = ["저스트 왁 & 비트","ぼっち・ざ・ろっく!","들로네 삼각분할","ひとりぼっち","비어있음"];
+    var works_url = ["https://github.com/Just-Wak-Beat/Project_Wak_Beat","https://github.com/ABER1047/bocchi","https://github.com/ABER1047/delaunay-trianglulation","https://github.com/ABER1047/HitoriBocchi"];
+    var works_title_eng = ["Project Wak beat","Bocchi!","Delaunay Triangulation","Hitori Bocchi","Empty"];
+    var works_title_kr = ["프로젝트 왁비트","ぼっち・ざ・ろっく!","들로네 삼각분할","ひとりぼっち","비어있음"];
 
 
     for(var i = -max_size_sub_project; i <= max_size_sub_project; i++)
